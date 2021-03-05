0ee4469a165177b6c6f699e17ae86c3b4b59aee4
Konstantin <hippielovesflowers@gmail.com>
2021-03-06 01:38:18 +0300

    Update NPCBase

--- Moujou.Calendar/Supports/NPCBase.cs
+++ Moujou.Calendar/Supports/NPCBase.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Moujou.Calendar.Supports
 {
     public class NPCBase : INotifyPropertyChanged
     {
-        // Implementation INotifyPropwertyChanged
+        // Implementation INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string prop = "")
+        public void OnPropertyChanged([CallerMemberName] string property = "")
         {
             if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
         }
     }
 }
