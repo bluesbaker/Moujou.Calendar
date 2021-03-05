9adf7bb30cd2f87ba826e13ab876f0abf59ed471
Konstantin <hippielovesflowers@gmail.com>
2021-03-05 23:57:13 +0300

    Update CalendarDay
    Now the CalendarDay is inherited from NPCBase

--- Moujou.Calendar/Models/CalendarDay.cs
+++ Moujou.Calendar/Models/CalendarDay.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Moujou.Calendar.Supports;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 
 namespace Moujou.Calendar.Models
 {
-    public class CalendarDay : INotifyPropertyChanged
+    public class CalendarDay : NPCBase
     {
         private int _numOfDay;
         public int NumOfDay
@@ -16,14 +17,6 @@ namespace Moujou.Calendar.Models
                 _numOfDay = value;
                 OnPropertyChanged("NumOfDay");
             }
-        }
-
-        // Implementation INotifyPropwertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
