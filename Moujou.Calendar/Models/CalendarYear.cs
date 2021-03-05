f9062794c423155a962628e6280b8e41bd154bca
Konstantin <hippielovesflowers@gmail.com>
2021-03-05 23:54:41 +0300

    Add CalendarYear

---
+++ Moujou.Calendar/Models/CalendarYear.cs
@@ -0,0 +1,33 @@
+﻿using Moujou.Calendar.Supports;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Moujou.Calendar.Models
+{
+    public class CalendarYear : NPCBase
+    {
+        private CalendarMonth[] _months;
+        public CalendarMonth[] Months
+        {
+            get => _months;
+            set
+            {
+                _months = value;
+                OnPropertyChanged("Months");
+            }
+        }
+
+        private int _numOfYear;
+        public int NumOfYear
+        {
+            get => _numOfYear;
+            set
+            {
+                _numOfYear = value;
+                OnPropertyChanged("NumOfYear");
+            }
+        }
+    }
+}
