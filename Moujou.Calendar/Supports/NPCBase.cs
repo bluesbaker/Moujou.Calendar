e710216feda5a00e7ef94cb4817a54e6eb017b91
Konstantin <hippielovesflowers@gmail.com>
2021-03-05 23:55:47 +0300

    Add NPCBase
    The NPCBase is a implementation of INotifyPropertyChanged

---
+++ Moujou.Calendar/Supports/NPCBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Moujou.Calendar.Supports
+{
+    public class NPCBase : INotifyPropertyChanged
+    {
+        // Implementation INotifyPropwertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
