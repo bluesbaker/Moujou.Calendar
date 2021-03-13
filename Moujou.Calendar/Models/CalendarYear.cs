0c147c5086eea5d40b133b8c8eb9584deda85f87
Konstantin <hippielovesflowers@gmail.com>
2021-03-13 22:46:53 +0300

    Update CalendarYear

--- Moujou.Calendar/Models/CalendarYear.cs
+++ Moujou.Calendar/Models/CalendarYear.cs
@@ -1,9 +1,5 @@
 ﻿using Moujou.Calendar.Supports;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Text;
 
 namespace Moujou.Calendar.Models
 {
