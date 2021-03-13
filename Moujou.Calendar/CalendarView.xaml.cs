fe40084e24d45a177abf2d573b87f442501ac68a
Konstantin <hippielovesflowers@gmail.com>
2021-03-13 22:47:53 +0300

    Update CalendarView

--- Moujou.Calendar/CalendarView.xaml.cs
+++ Moujou.Calendar/CalendarView.xaml.cs
@@ -55,7 +55,7 @@ namespace Moujou.Calendar
             set
             {
                 _currentYear = value;
-                OnPropertyChanged("CurrentYear");
+                OnPropertyChanged();
             }
         }
 
