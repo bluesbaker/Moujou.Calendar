58a3b8c7a8d97a259ca05fb2374faa12140826da
Konstantin <hippielovesflowers@gmail.com>
2021-03-19 22:38:23 +0300

    Add CalendarDate

---
+++ Moujou.Calendar/Models/CalendarDate.cs
@@ -0,0 +1,21 @@
+﻿using Moujou.Calendar.Supports;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moujou.Calendar.Models
+{
+    public class CalendarDate : NPCBase
+    {
+        private CalendarYear _calendarYear;
+        public CalendarYear CalendarYear
+        {
+            get => _calendarYear;
+            set
+            {
+                _calendarYear = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
