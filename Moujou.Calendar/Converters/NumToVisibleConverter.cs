24d36094c6c8223eb45bfbfbecc0109955f5eeff
Konstantin <49731374+bluesbaker@users.noreply.github.com>
2021-04-06 00:00:17 +0300

    Update NumToVisibleConverter.cs

--- Moujou.Calendar/Converters/NumToVisibleConverter.cs
+++ Moujou.Calendar/Converters/NumToVisibleConverter.cs
@@ -13,7 +13,7 @@ namespace Moujou.Calendar.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
