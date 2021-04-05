4773e68619d365efbb5f362ad48c3537fb6258f8
Konstantin <49731374+bluesbaker@users.noreply.github.com>
2021-04-05 23:59:35 +0300

    Update NPCBase.cs

--- Moujou.Calendar/Supports/NPCBase.cs
+++ Moujou.Calendar/Supports/NPCBase.cs
@@ -5,7 +5,6 @@ namespace Moujou.Calendar.Supports
 {
     public class NPCBase : INotifyPropertyChanged
     {
-        // Implementation INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string property = "")
         {
